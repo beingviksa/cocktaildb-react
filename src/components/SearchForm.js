bdd133d3a40f45b38474b2341a850d1388b7b46d
Vikram Singh <singhvikram143777@gmail.com>
2023-09-20 18:20:06 +0530

    edited serch title

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -16,7 +16,7 @@ const SearchForm = ({ setSearchTerm }) => {
   };
   return (
     <section className="section">
-      <h2 className="section-title">search cocktails</h2>
+      <h2 className="section-title">search cocktails: testing</h2>
       <form className="form search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
